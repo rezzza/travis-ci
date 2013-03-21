97c60e0a14772dbcf8624da72c0906e9ccf7eada
Sébastien HOUZÉ <sebastien.houze@gmail.com>
2013-03-21 03:07:50 +0100

    fix

--- composer/install.sh
+++ composer/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p $HOME/bin \
-    && curl -s https://getcomposer.org/installer | php -- --install-dir=$HOME/bin \
-    && export PATH=$HOME/bin:$PATH
+    && curl -s https://getcomposer.org/installer | php -- --install-dir=$HOME/bin
+
+export PATH=$HOME/bin:$PATH
