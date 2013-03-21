f52b6a58a04a5fbbcaa068c74daab36dedc9e075
Sébastien HOUZÉ <s@verylastroom.com>
2013-03-21 13:58:16 +0100

    rollback by now to old way for ms tt core font installation.

--- msttcorefonts/install.sh
+++ msttcorefonts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
-sudo apt-get install ttf-mscorefonts-installer > /dev/null
+#sudo sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
+#sudo apt-get install ttf-mscorefonts-installer > /dev/null
 
-#mkdir -p ~/.downloads/debs \
-#    && curl http://imaginux.com/repos/pool/renzo/msttcorefonts-offline_1.0-0ubuntu1_all.deb \
-#    -o ~/.downloads/msttcorefonts-offline_1.0-0ubuntu1_all.deb \
-#    && sudo dpkg -i ~/.downloads/msttcorefonts-offline_1.0-0ubuntu1_all.deb
+mkdir -p ~/.downloads/debs \
+    && curl http://imaginux.com/repos/pool/renzo/msttcorefonts-offline_1.0-0ubuntu1_all.deb \
+    -o ~/.downloads/msttcorefonts-offline_1.0-0ubuntu1_all.deb \
+    && sudo dpkg -i ~/.downloads/msttcorefonts-offline_1.0-0ubuntu1_all.deb
