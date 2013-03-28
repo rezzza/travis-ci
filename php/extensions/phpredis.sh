af00f76869b27bc2e0be10a257064a47effb8587
Sébastien HOUZÉ <s@verylastroom.com>
2013-03-28 11:28:55 +0100

    Fixing phpredis compilation error.

--- php/extensions/phpredis.sh
+++ php/extensions/phpredis.sh
@@ -2,6 +2,7 @@
 mkdir -p ~/.rezzza/php/extensions && cd ~/.rezzza/php/extensions \
     && git clone --depth=50 https://github.com/nicolasff/phpredis \
     && cd phpredis \
+    && git checkout b2ffb7ac753f5fa94d808dbf684a64e9f0e6f8c5 \
     && phpize \
     && echo "Configuring phpredis..." \
     && ./configure > /dev/null \
