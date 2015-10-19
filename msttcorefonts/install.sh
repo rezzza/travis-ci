7f284de5758e565e09e503ebeafc74cef912d08d
Timothée Barray <tim@amicalement-web.net>
2015-10-19 10:48:00 +0200

    Fix msttcorefonts install to use with trusty build

--- msttcorefonts/install.sh
+++ msttcorefonts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
-sudo apt-get install ttf-mscorefonts-installer > /dev/null
+sudo aptitude -y install ttf-mscorefonts-installer > /dev/null
 
 #mkdir -p ~/.downloads/debs \
     #&& curl http://imaginux.com/repos/pool/renzo/msttcorefonts-offline_1.0-0ubuntu1_all.deb \
