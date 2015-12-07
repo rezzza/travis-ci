9a9b76b523b8677ed74859ea5586d76115e8ddb9
Timothée Barray <tim@amicalement-web.net>
2015-12-07 11:44:19 +0100

    Create config apcu for php 5.6

---
+++ php/extensions/apcu-5.6.sh
@@ -0,0 +1,1 @@
+echo "yes\nno\n" | pecl -d preferred_state=stable install apcu-4.0.10 && phpenv config-add ~/.rezzza.travis-ci/php/conf.d/apcu.ini && phpenv config-add ~/.rezzza.travis-ci/php/conf.d/opcode.ini
