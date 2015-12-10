98c57186e96da762962907a6d28aaf30b32fe9ff
Sébastien HOUZÉ <sebastien.houze@gmail.com>
2015-12-10 11:42:02 +0100

    Update pickle-bootstrap.sh

--- php/pickle-bootstrap.sh
+++ php/pickle-bootstrap.sh
@@ -3,6 +3,7 @@ git clone https://github.com/FriendsOfPHP/pickle.git \
   && pushd . \
   && cd pickle \
   && git reset --hard 5ad1daf0396174d96d616b8b0b05c0979b85fd7d \
+  && composer install --no-dev --optimize-autoloader \
   && curl -LSs http://box-project.github.io/box2/installer.php | php \
   && php -d phar.readonly=0 box.phar build \
   && sudo ln -s $(pwd)/bin/pickle /usr/bin/pickle \
