2a50727326fd9f2669f05e5c2e90cf83f15ba1c0
Sébastien HOUZÉ <sebastien.houze@gmail.com>
2015-12-10 13:05:39 +0100

    Update bootstrap-trusty.sh

--- apache/bootstrap-trusty.sh
+++ apache/bootstrap-trusty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -q -y --force-yes apache2 libapache2-mod-fastcgi \
   && sudo a2enmod actions auth_basic auth_digest deflate fastcgi headers rewrite vhost_alias \
-  && sudo a2dissite default 000-default \
+  && sudo a2dissite 000-default \
   && sudo chmod a+rx /home/travis \
   && sudo mkdir -p /etc/apache2/conf.d/ \
   && sudo echo "Listen $1" | sudo tee /etc/apache2/ports.conf \
