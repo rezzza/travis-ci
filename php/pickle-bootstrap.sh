d235e69dec6c6e520a7314e7da7d2b309ab35924
Sébastien HOUZÉ <sebastien.houze@gmail.com>
2015-12-10 12:02:29 +0100

    Update pickle-bootstrap.sh

--- php/pickle-bootstrap.sh
+++ php/pickle-bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git clone https://github.com/FriendsOfPHP/pickle.git \
-  && pushd . \
+pushd . \
+  && cd ~/ \
+  && git clone https://github.com/FriendsOfPHP/pickle.git \
   && cd pickle \
   && git reset --hard 5ad1daf0396174d96d616b8b0b05c0979b85fd7d \
   && composer install --no-dev --optimize-autoloader \
