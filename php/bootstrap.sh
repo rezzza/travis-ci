55490120184c3228d9c499adc378c8401f2c2b79
Sébastien HOUZÉ <s@verylastroom.com>
2015-12-11 15:16:36 +0100

    Add phpredis extension bootstrap for php.

--- php/bootstrap.sh
+++ php/bootstrap.sh
@@ -24,7 +24,7 @@ bootstrap_pickle()
           && git clone https://github.com/FriendsOfPHP/pickle.git \
           && cd pickle \
           && git reset --hard 5ad1daf0396174d96d616b8b0b05c0979b85fd7d \
-          && composer install --no-dev --optimize-autoloader \
+          && composer install --no-dev --optimize-autoloader --prefer-dist \
           && curl -LSs http://box-project.github.io/box2/installer.php | php \
           && php -d phar.readonly=0 box.phar build \
           && sudo ln -s $(pwd)/bin/pickle /usr/bin/pickle \
@@ -38,13 +38,14 @@ bootstrap_apcu()
     # manage apcu install subtilities between php >= 7.0 and others
     if [ $(phpenv version-name) = "7.0" ]; then
       pickle install --defaults apcu-5.1.2 \
+        && mkdir -p ~/.rezzza/php/extensions \
         && pushd . \
-        && cd ~/ \
+        && cd ~/.rezzza/php/extensions \
         && git clone https://github.com/krakjoe/apcu-bc.git \
         && cd apcu-bc \
         && git reset --hard 078a5742bed918665e66ac4f37583edc89a3e506 \
         && popd \
-        && pickle install --defaults ~/apcu-bc
+        && pickle install --defaults ~/.rezzza/php/extensions/apcu-bc
     else
       pickle install --defaults apcu-4.0.10
     fi
@@ -56,6 +57,25 @@ bootstrap_apcu()
     fi
 
     phpenv config-add ~/.rezzza.travis-ci/php/conf.d/apc.ini
+}
+
+bootstrap_redis()
+{
+    if [ $(phpenv version-name) = "7.0" ]; then
+        mkdir -p ~/.rezzza/php/extensions \
+            && pushd . \
+            && cd ~/.rezzza/php/extensions \
+            && git clone --branch php7 --depth=10 https://github.com/shouze/phpredis \
+            && cd phpredis \
+            && popd \
+            && pickle install --defaults ~/.rezzza/php/extensions/phpredis
+    else
+        if [ $(php -m | grep -i redis | wc -l) -eq 0 ]; then
+            pickle install --defaults redis-2.2.7
+        fi
+    fi
+
+    phpenv config-add ~/.rezzza.travis-ci/php/conf.d/redis.ini
 }
 
 bootstrap_fpm()
@@ -87,6 +107,9 @@ bootstrap_base \
                ;;
                apcu)
                    bootstrap_apcu
+               ;;
+               redis)
+                   bootstrap_redis
                ;;
                *)
                    printf "\nERROR: '%s' option is not implemented yet\n" $arg
