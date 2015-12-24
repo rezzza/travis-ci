485bc716ed11e53758dddd9c2ef05ca25fa0e16c
Sébastien HOUZÉ <sebastien.houze@gmail.com>
2015-12-24 15:43:49 +0100

    Update bootstrap.sh

--- varnish/bootstrap.sh
+++ varnish/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl https://repo.varnish-cache.org/GPG-key.txt | sudo apt-key add - \
-    && echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.0" | sudo tee -ai /etc/apt/sources.list.d/varnish-cache.list \
+    && printf "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.0\n" | sudo tee -ai /etc/apt/sources.list.d/varnish-cache.list \
     && sudo apt-get update -qq \
     && sudo apt-get install -q -y --force-yes varnish \
     && sudo cp ~/.rezzza.travis-ci/varnish/libvmod_curl.so /usr/lib/varnish/vmods/libvmod_curl.so \
